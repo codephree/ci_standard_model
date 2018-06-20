940542ad289f3f67886afb8a4bcce65b1907f937
codephree <odeleyeoluwafemi@gmail.com>
2018-06-20 21:55:28 +0100

    Update the standard model

---
+++ application/models/StandardModel.php
@@ -0,0 +1,58 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+/**
+ * CodeIgniter Auth Class
+ *
+ * Model to manage authentication attempts
+ *
+ * @package 	CodeIgniter
+ * @subpackage 	Stabdard Model
+ * @category 	Library
+ * @author 		Odeleye Oluwafemi (Codephree)
+ * @version 	0.1.1
+ */
+class StandardModel extends CI_MODEL {
+
+   // Declare table variable
+    private $table;
+		
+	function __construct()
+	{
+		/*
+		 *  Assigning Table to the variable
+		 */
+		$this->table = "Your Table Name";
+	}
+
+
+	/*
+	 * Select query from the table 
+	 */
+	public function select_from_where($id)
+	{
+		  
+		  $select = $this->db->select('*')
+		  					->where('id',$id)
+		                     ->get($this->table);
+
+		  return  $select->result();
+	}
+
+	/*
+	 * Insert into table
+	 * Note $data is an array with columnname
+	 * is the key  
+	 */
+
+	 public function Create($data)
+	 {
+		 $insert = $this->db->insert($this->table, $data);
+		 if($insert)
+		 {
+		 	return json_encode($data)
+         }
+		 
+	 }
+
+
+
+}
